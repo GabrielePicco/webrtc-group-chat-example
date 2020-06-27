f10f792cd0945cb280149ba70a74654a1b568241
Gabriele Picco <piccogabriele@gmail.com>
2020-06-27 16:57:58 +0200

    Update signaling-server.js

--- signaling-server.js
+++ signaling-server.js
@@ -2,6 +2,7 @@
 /*** CONFIG ***/
 /**************/
 
+PORT = 80;
 
 /*************/
 /*** SETUP ***/
@@ -14,7 +15,9 @@ var server = http.createServer(main)
 var io  = require('socket.io').listen(server);
 //io.set('log level', 2);
 
-
+server.listen(PORT, null, function() {
+    console.log("Listening on port " + PORT);
+});
 //main.use(express.bodyParser());
 
 main.get('/', function(req, res){ res.sendFile(__dirname + '/client.html'); });
